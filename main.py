382f1564392df40504eac038f812171760100d4a
Eric Brown <ecbrown603@gmail.com>
2021-03-25 18:34:50 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_products = pd.read_csv('summer-products-with-rating-and-performance_2020-08.csv')
+df_uc = pd.read_csv('unique-categories.csv')
+df_ucs = pd.read_csv('unique-categories.sorted-by-count.csv')
+
